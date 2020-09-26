f3f8921eae7a69a4c9677b046809d9a5ac28c34a
Heath Burton <rubickdev@gmail.com>
2020-09-26 14:04:54 +1000

    trying again

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php include_once("html/members/members.html"); ?>
-<?php include_once("html/members/members_orders.html"); ?>
-<?php include_once("html/members/stylers.css"); ?>
-<?php include_once("html/members/styles2.css"); ?>
+<?php header( 'location: html/members/members.html'); ?>
